c328ed8f68c46a2d8986e09222e750eb680013c9
rhedwan <adesolaridwan2003@gmail.com>
2024-01-19 16:47:52 +0100

    webhook

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PaymentLinkCreateListView
+from .views import PaymentLinkCreateListView, webhook_endpoint
+
 
 urlpatterns = [
     path(
         "paymentlink/", PaymentLinkCreateListView.as_view(), name="create_payment_link"
-    )
+    ),
+    path("webhook-url/", webhook_endpoint, name="webhook_endpoint"),
 ]
--- apps/payment/views.py
+++ apps/payment/views.py
@@ -4,6 +4,52 @@ from .models import PaymentLink
 from rest_framework.permissions import IsAuthenticated
 import requests
 
+# views.py within your Django app
+
+import hmac
+import hashlib
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.encoding import force_bytes
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = env("SECRET_KEY")
+
+
+@require_POST
+@csrf_exempt
+def webhook_endpoint(request):
+    # Ensure you have the 'json' body-parser middleware applied
+    try:
+        request_body = force_bytes(request.body.decode("utf-8"))
+        received_signature = request.headers.get("x-squad-encrypted-body", "")
+        expected_signature = (
+            hmac.new(
+                force_bytes(SECRET_KEY), msg=request_body, digestmod=hashlib.sha512
+            )
+            .hexdigest()
+            .upper()
+        )
+
+        if received_signature == expected_signature:
+            # You can trust the event came from Squad
+            # Process the event here
+            print(expected_signature)
+            return JsonResponse({"status": "success"}, status=200)
+        else:
+            # The request didn't come from Squad, ignore it
+            return JsonResponse({"status": "invalid_signature"}, status=400)
+    except Exception as e:
+        return JsonResponse({"status": "error", "message": str(e)}, status=400)
+
 
 class PaymentLinkCreateListView(generics.ListCreateAPIView):
     queryset = PaymentLink.objects.all()
